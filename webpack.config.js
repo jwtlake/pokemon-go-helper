9caa0af4c69712ccd68204257c238427e5362cc2
Jonathan Lake <jwtlake@gmail.com>
2016-07-24 18:54:39 -0700

    updated index extension to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
