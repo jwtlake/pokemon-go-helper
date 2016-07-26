52e273e75b764a82322d139498f9a7fe96302c69
Jonathan Lake <jwtlake@gmail.com>
2016-07-25 23:27:28 -0700

    fixed key name to reflect real data

--- src/client/reducers/pokedex.js
+++ src/client/reducers/pokedex.js
@@ -6,7 +6,7 @@ function pokedex(state = initialState, action) {
 	switch (action.type) {
 		case types.LOAD:
 			return action.pokedex.reduce((map, pokemon) => {
-				map[pokemon.PokedexEntryNumber.toString()] = pokemon;
+				map[pokemon.pokemon_id.toString()] = pokemon;
 				return map;	
 			},{});
         	default: // default return on unknown action type
